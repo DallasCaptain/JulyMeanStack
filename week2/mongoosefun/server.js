24099ac66747c6dff651c52c7466adfa28f097af
Chris Thompson <cthompson@codingdojo.com>
2020-07-14 23:34:56 -0500

    W2D2 Lecture - Login/Reg

--- week2/mongoosefun/server.js
+++ week2/mongoosefun/server.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose')
-
+const session = require('express-session')
+const flash = require('express-flash');
 mongoose.connect('mongodb://localhost/mongofun', {useNewUrlParser: true})
+
 const ShoeSchema = new mongoose.Schema({
-    name: String
+    name: String,
+    
 })
 const Shoe = mongoose.model('Shoe', ShoeSchema)
 
-const UserSchema = new mongoose.Schema({
+const WardrobeSchema = new mongoose.Schema({
     name: String,
+    articles: [ShoeSchema]
+})
+
+const Wardrobe = mongoose.model('Wardrobe', WardrobeSchema)
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
     age: { type: Number, required: true },
+    wardrobe: WardrobeSchema,
     favShoe: ShoeSchema,
 })
 
@@ -18,19 +29,24 @@ const User = mongoose.model('User', UserSchema)
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/static"));
-
-
+app.use(flash())
+app.use(session({
+    secret: 'I solemly swear i am upto no good',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  }))
 
 app.set('view engine', 'ejs');  
 app.set('views', __dirname + '/views');
 
 
 app.get('/', (req, res) => {
-    
+    var errors = req.flash('errors')
     User.find()
     .then(data => {
         console.log('data from .then',data)
-        res.render('users', {users: data});
+        res.render('users', {users: data,errors:errors});
     })
     console.log('after .then')
 });
@@ -49,7 +65,15 @@ app.post('/users', (req,res) => {
             res.redirect('/')
         })
         .catch(err => {
-            console.log('err:', err)
+            console.log('err:', err.errors.age)
+            if(err.errors.name){
+                req.flash('errors','Name is required')
+            }
+            if(err.errors.age){
+                
+                req.flash('errors','Age is required')
+            }
+            
             res.redirect('/')
         })
     })
@@ -79,9 +103,23 @@ app.get('/funny', (req,res)=>{
 app.get('/initshoes', (req,res)=>{
     newshoe = new Shoe()
     newshoe.name = 'mykicks'
+
+    othershoe = new Shoe()
+    othershoe.name = 'othershoe'
     newshoe.save()
-    .then(()=>{
-        res.redirect('/')
+    .then((shoe1)=>{
+        othershoe.save()
+        .then((shoe2)=>{
+            newwardrobe = new Wardrobe()
+            newwardrobe.name = 'bobs clothes'
+            newwardrobe.articles = [shoe1,shoe2]
+            newwardrobe.save()
+            .then((wd)=>{
+                console.log('new wardrobe: ',wd)
+                res.redirect('/')
+            })
+        })
+        
     })
 })
 
